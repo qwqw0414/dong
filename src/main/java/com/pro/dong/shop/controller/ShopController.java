9b638351b9ae2da1419e7b6a83fe60736a1dd17c
신하진 <gkwls5865@naver.com>
2020-02-03 15:02:43 +0900

    내상점보기 jsp 설정

--- src/main/java/com/pro/dong/shop/controller/ShopController.java
+++ src/main/java/com/pro/dong/shop/controller/ShopController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.pro.dong.member.controller.MemberController;
 import com.pro.dong.member.model.service.MemberService;
@@ -27,7 +28,11 @@ public class ShopController {
 	
 	
 	// 하진 시작 ==========================
-	
+	@RequestMapping("/myShopView.do")
+	public ModelAndView myshopView(ModelAndView mav) {
+		mav.setViewName("/shop/myShopView");
+		return mav;
+	}
 	
 	
 	//========================== 하진 끝
