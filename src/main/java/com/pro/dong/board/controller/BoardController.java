0fb29e2bdc279a2c3a0dd1a75f9a8ee49a60c4e7
김민호 <v.i.c@hanmail.net>
2020-02-04 09:55:52 +0900

    board 어노테이션

--- src/main/java/com/pro/dong/board/controller/BoardController.java
+++ src/main/java/com/pro/dong/board/controller/BoardController.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pro.dong.board.model.service.BoardService;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 	
 	static Logger log = LoggerFactory.getLogger(BoardController.class);
@@ -16,7 +18,10 @@ public class BoardController {
 	
 	
 	// 민호 시작 ==========================
-	
+	@RequestMapping("/boardList.do")
+	public void boardList() {
+		
+	}
 	//==========================민호 끝
 		
 	// 하진 시작 ==========================
---
+++ src/main/webapp/WEB-INF/views/board/boardList.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<jsp:include page="/WEB-INF/views/common/header.jsp"/>
+
+ <h1>커뮤니티 게시판</h1>
+   
+<jsp:include page="/WEB-INF/views/common/footer.jsp"/>
