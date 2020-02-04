57adb282a616bf804850742160033b9fe8237876
최주영 <c2238571@naver.com>
2020-02-04 15:19:11 +0900

    no message

--- src/main/java/com/pro/dong/shop/controller/ShopController.java
+++ src/main/java/com/pro/dong/shop/controller/ShopController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pro.dong.member.controller.MemberController;
@@ -74,9 +75,13 @@ public class ShopController {
 		mav.addObject("map", map);
 		
 		return mav;
-		
-		
 	}
+	
+	/*@RequestMapping("/shop/updateShopInfo")
+	public Shop shopInfoUpdate(@RequestParam("memberId") String memberId) {
+		
+		
+	}*/
 	
 	
 	
