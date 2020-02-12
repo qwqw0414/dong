b16283aaa1cfb17f3fff3719ab4ccbcb7e719f54
choijuyoung8571 <c2238571@gmail.com>
2020-02-12 23:35:54 +0900

    no message

--- src/main/java/com/pro/dong/admin/model/dao/AdminDAO.java
+++ src/main/java/com/pro/dong/admin/model/dao/AdminDAO.java
@@ -26,8 +26,6 @@ public interface AdminDAO {
 	
 	int memberDelete(String memberId);
 	// ========================== 하진 끝
-	
-
 
 	// 근호 시작 ==========================
 
@@ -42,7 +40,8 @@ public interface AdminDAO {
 	// ========================== 예찬 끝
 
 	// 주영 시작 ==========================
-
+	int selectBoardTotalContents(Map<String, String> param);
+	List<Product> loadBoardList(int cPage, int numPerPage, Map<String, String> param);
 	// ========================== 주영 끝
 
 	// 현규 시작 ==========================
